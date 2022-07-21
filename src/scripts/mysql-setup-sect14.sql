8db176238eceac9b2e0aae1d00a90bb1caa53ea8
PierreSQS <nomo_messina@web.de>
2022-07-21 20:03:11 +0200

    Sect14_Chap117-Hibernate Code Review: update SQL-Script

--- src/scripts/mysql-setup-sect14.sql
+++ src/scripts/mysql-setup-sect14.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS bookdb2;
-DROP USER IF EXISTS `bookadmin`@`%`;
-DROP USER IF EXISTS `bookuser`@`%`;
-CREATE DATABASE IF NOT EXISTS bookdb2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `bookadmin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+DROP DATABASE IF EXISTS jt_hibernate_jpa_bookdb2;
+DROP USER IF EXISTS `bookadmin2`@`%`;
+DROP USER IF EXISTS `bookuser2`@`%`;
+CREATE DATABASE IF NOT EXISTS jt_hibernate_jpa_bookdb2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS `bookadmin2`@`%` IDENTIFIED WITH mysql_native_password BY 'bookadmin2';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `bookdb2`.* TO `bookadmin`@`%`;
-CREATE USER IF NOT EXISTS `bookuser`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `bookdb2`.* TO `bookuser`@`%`;
+    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `jt_hibernate_jpa_bookdb2`.* TO `bookadmin2`@`%`;
+CREATE USER IF NOT EXISTS `bookuser2`@`%` IDENTIFIED WITH mysql_native_password BY 'bookuser2';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `jt_hibernate_jpa_bookdb2`.* TO `bookuser2`@`%`;
 FLUSH PRIVILEGES;
