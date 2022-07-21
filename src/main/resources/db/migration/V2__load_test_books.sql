0eb9aaaad550f6f757cc3e3f757f368ab5482f48
PierreSQS <nomo_messina@web.de>
2022-07-21 16:24:19 +0200

    Sect14_Chap117-Hibernate Code Review: Pierrot DB settings

---
+++ src/main/resources/db/migration/V2__load_test_books.sql
@@ -0,0 +1,120 @@
+insert into book (isbn, publisher, title, author_id) values ('978-1617294945', 'Simon & Schuster',
+       'Spring in Action, 5th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292545', 'Simon & Schuster',
+    'Spring Boot in Action, 1st Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297571', 'Simon & Schuster',
+    'Spring in Action, 6th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125217', 'Addison Wesley',
+    'Domain-Driven Design',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134494167', 'Addison Wesley',
+    'Clean Code 2nd',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294946', 'Simon & Schuster',
+       'Kubernetes in Action, 5th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292547', 'Simon & Schuster',
+    'Spring Data JPA in Action, 1st Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297572', 'Simon & Schuster',
+    'Spring in Action, 7th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125218', 'Addison Wesley',
+    'Domain-Driven Design 2nd Edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134494169', 'Addison Wesley',
+    'Clean Code in Java',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294951', 'Simon & Schuster',
+       'Kubernetes with Spring, 4th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292552', 'Simon & Schuster',
+    'Kubernetes with Spring, 1st Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297553', 'Simon & Schuster',
+    'Kubernetes with Spring, 2nd Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125254', 'Addison Wesley',
+    'Microservices, 1st Edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134494155', 'Addison Wesley',
+    'Clean Code, 3rd Edition',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294956', 'Simon & Schuster',
+       'Kubernetes in Action, 3th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292557', 'Simon & Schuster',
+    'Spring Data JPA in Action, 2st Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297558', 'Simon & Schuster',
+    'Spring Data JPA in Action, 3rd Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125259', 'Addison Wesley',
+    'Domain-Driven Design 3rd Edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134494160', 'Addison Wesley',
+    'Clean Code in Java Version2',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294922', 'Simon & Schuster',
+       'Spring Boot in Action, 5th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292523', 'Simon & Schuster',
+    'Spring Boot in Action, Premium',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297524', 'Simon & Schuster',
+    'Spring in Action, Premium',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125225', 'Addison Wesley',
+    'Domain-Driven Design,Premium',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134494126', 'Addison Wesley',
+    'Clean Code 2nd, Edition Premium',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294927', 'Simon & Schuster',
+       'Kubernetes in Action, Premium',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292528', 'Simon & Schuster',
+    'Spring Data JPA in Action, Premium',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297529', 'Simon & Schuster',
+    'Spring5 in Action',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125230', 'Addison Wesley',
+    'Domain-Driven Design 3rd Edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134494131', 'Addison Wesley',
+    'Clean Code in Java, Premium',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294932', 'Simon & Schuster',
+       'Kubernetes with Spring6, 4th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292533', 'Simon & Schuster',
+    'Modern Kubernetes with Spring, 1st Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297534', 'Simon & Schuster',
+    'Kubernetes for experts, 2nd Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125235', 'Addison Wesley',
+    'Microservices for architects, 1st Edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134494136', 'Addison Wesley',
+    'Clean Code in Java, 3rd Edition',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294937', 'Simon & Schuster',
+       'Modern Kubernetes, 3th Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617292538', 'Simon & Schuster',
+    'Spring Cloud in Action, 2st Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617297539', 'Simon & Schuster',
+    'Spring Data JPA with Oracle, 3rd Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125240', 'Addison Wesley',
+    'Domain-Driven Design Premium',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0134496041', 'Addison Wesley',
+    'Clean Code in Spring6',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
